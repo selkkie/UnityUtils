1560ee5f79d56ee65d1d5c6e05ffef579a92648d
Selkira <Selkira@selkie.is>
2020-05-23 10:32:14 -0400

    Added more utils

---
+++ ExtensionMethods.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+
+    //rgb/hex convertor from taylor-libonati on answers.unity.com
+    //modified from http://wiki.unity3d.com/index.php?title=HexConverter&_ga=2.166577470.1028837474.1589917001-493193805.1583368431
+    public static string colorToHex(Color32 color)
+    {
+        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+        return hex;
+    }
+
+    public static Color hexToColor(string hex)
+    {
+        hex = hex.Replace("0x", "");//in case the string is formatted 0xFFFFFF
+        hex = hex.Replace("#", "");//in case the string is formatted #FFFFFF
+        byte a = 255;//assume fully visible unless specified in hex
+        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        //Only use alpha if the string has enough characters
+        if (hex.Length == 8)
+        {
+            a = byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
+        }
+        return new Color32(r, g, b, a);
+    }
+
+
+    //from ben-rassooli on answers.unity.com
+    public static void AddListener(this EventTrigger trigger, EventTriggerType eventType, System.Action<PointerEventData> listener)
+    {
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = eventType;
+        entry.callback.AddListener(data => listener.Invoke((PointerEventData)data));
+        trigger.triggers.Add(entry);
+    }
+}
